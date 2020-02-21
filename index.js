9756829420e58d6c52f879711fba2423f0d104ef
dwstudios <theChawke@gmail.com>
2020-02-21 17:34:35 +0100

    cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
 
 const templates = require('./routes/templates')
 // app.use(express.static('../public'))
 app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors())
+
 templates(app)
 
 const PORT = process.env.PORT || 3003
