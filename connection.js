96b49ae0b0195d19e1c2b9bf2a2e67c84fc577b5
dwstudios <theChawke@gmail.com>
2020-06-18 13:30:29 +0200

    0.3-6

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// // user heroku_h4f3xwnf
-// // pass 1p3tq6gev7p4bgpfvqtacasgav
-// // host ds139167.mlab.com:39167
-// // db heroku_h4f3xwnf
-// //? heroku config:get MONGODB_URI
+// user heroku_h4f3xwnf
+// pass 1p3tq6gev7p4bgpfvqtacasgav
+// host ds139167.mlab.com:39167
+// db heroku_h4f3xwnf
+//? heroku config:get MONGODB_URI
 const dburl = 'mongodb://heroku_h4f3xwnf:1p3tq6gev7p4bgpfvqtacasgav@ds139167.mlab.com:39167/heroku_h4f3xwnf';
 mongoose.connect(dburl, {
     useNewUrlParser: true,
