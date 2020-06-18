2bff887fe4470babecc869bb32cd2f4ec86a909d
dwstudios <theChawke@gmail.com>
2020-06-18 13:28:37 +0200

    0.3-5

--- connection.js
+++ connection.js
@@ -5,8 +5,8 @@
 // // host ds139167.mlab.com:39167
 // // db heroku_h4f3xwnf
 // //? heroku config:get MONGODB_URI
-// const dburl = 'mongodb://heroku_h4f3xwnf:1p3tq6gev7p4bgpfvqtacasgav@ds139167.mlab.com:39167/heroku_h4f3xwnf';
-// mongoose.connect(dburl, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
+const dburl = 'mongodb://heroku_h4f3xwnf:1p3tq6gev7p4bgpfvqtacasgav@ds139167.mlab.com:39167/heroku_h4f3xwnf';
+mongoose.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
--- index.js
+++ index.js
@@ -6,41 +6,29 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const session = require('express-session')
-// const MongoDBStore = require('connect-mongodb-session')(session);
 const MongoStore = require('connect-mongo')(session);
 const mongoose = require('mongoose');
-// const flash = require('express-flash')
 const passport = require('passport')
 const app = express()
 
-// const db = require('./connection.js')
+const db = require('./connection.js')
 
 const users = require('./routes/users')
 const templates = require('./routes/templates')
 
-const dburl = 'mongodb://heroku_h4f3xwnf:1p3tq6gev7p4bgpfvqtacasgav@ds139167.mlab.com:39167/heroku_h4f3xwnf';
-mongoose.connect(dburl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
 // app.use(express.static('../public'))
 app.use(bodyParser.json());
-// app.use(bodyParser.text());
-// app.use(cors());
-// app.options('*', cors())
 
 app.use(session({
     secret: 'waq89ufaiw3r09jarfipkfa0sa',
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection })
-    // store: new MongoDBStore({ mongooseConnection: mongoose.connection })
 }))
 
 app.use(cors({
     origin: true,
     credentials: true,
-    // methods: true
 }))
 app.use(function (req, res, next) {
     // res.header("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
@@ -54,21 +42,15 @@ app.use(function (req, res, next) {
         next();
     }
 });
-// app.use(flash());
+
 app.use(passport.initialize())
 app.use(passport.session())
 
 users(app)
 templates(app)
 
-// const PORT = process.env.PORT || 3003
-// app.listen(PORT, () => {
-//     console.log('Server running on:', PORT);
-// })
-
 app.set( 'port', ( process.env.PORT || 3003 ));
 
-// Start node server
 app.listen( app.get( 'port' ), function() {
   console.log( 'Node server is running on port ' + app.get( 'port' ));
 });
